5cc52a8fb5aa43b1fabbe451d0f6c2a42555d407
Vaishnavi <vaishnavip2405@gmail.com>
2026-07-06 19:28:03 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const server = require('express');
+
+const app = server();
+
+app.get('/send',(req,res)=>{
+    res.send('Hello World');
+});
+
+const PORT = 8080;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
