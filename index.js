0c305fdaae1af14024f7ff373c94df91a18b01bf
Vaishnavi <vaishnavip2405@gmail.com>
2026-07-15 18:22:42 +0530

    added setup

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const server = require('express');
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./src/config/sqlConnection");
+const { auth, authroutes } = require("./src/routes/auth.route");
+const jwtVerify = require("./src/middelware/jwtVerify");
 
-const app = server();
+const app = express();
 
-app.get('/send',(req,res)=>{
-    res.send('Hello World');
+app.use(express.json());
+const PORT = process.env.PORT;
+
+console.log("port", PORT);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("database connected", sequelize.config.database);
+  })
+  .catch((error) => console.log(error));
+
+app.use(authroutes);
+app.use(jwtVerify);
+//server lisning on
+app.listen(PORT, () => {
+  console.log(`server running  on port-${PORT}`);
 });
-
-const PORT = 8080;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
